44ce5b06057900cc19ca67e6d22dc156162553ce
Victor Velasco <victor.velasco@student.swic.edu>
2019-01-29 12:10:55 -0600

    Jan 29 update

--- Homework/Assignment2/Calculator/Form1.Designer.vb
+++ Homework/Assignment2/Calculator/Form1.Designer.vb
@@ -34,61 +34,70 @@ Partial Class Form1
         '
         'Label1
         '
-        Me.Label1.Location = New System.Drawing.Point(32, 33)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(9, 49)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(67, 13)
+        Me.Label1.Size = New System.Drawing.Size(84, 13)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "First number:"
-        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'Label2
         '
-        Me.Label2.Location = New System.Drawing.Point(156, 33)
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(9, 75)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(86, 13)
+        Me.Label2.Size = New System.Drawing.Size(100, 13)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Second number:"
-        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'txtFirstNumber
         '
-        Me.txtFirstNumber.Location = New System.Drawing.Point(32, 49)
+        Me.txtFirstNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtFirstNumber.Location = New System.Drawing.Point(115, 46)
         Me.txtFirstNumber.Name = "txtFirstNumber"
-        Me.txtFirstNumber.Size = New System.Drawing.Size(67, 20)
+        Me.txtFirstNumber.Size = New System.Drawing.Size(142, 20)
         Me.txtFirstNumber.TabIndex = 2
+        Me.txtFirstNumber.Text = "21"
         Me.txtFirstNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'txtSecondNumber
         '
-        Me.txtSecondNumber.Location = New System.Drawing.Point(159, 50)
+        Me.txtSecondNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSecondNumber.Location = New System.Drawing.Point(115, 72)
         Me.txtSecondNumber.Name = "txtSecondNumber"
-        Me.txtSecondNumber.Size = New System.Drawing.Size(83, 20)
+        Me.txtSecondNumber.Size = New System.Drawing.Size(142, 20)
         Me.txtSecondNumber.TabIndex = 3
+        Me.txtSecondNumber.Text = "77"
         Me.txtSecondNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'btnAdd
         '
-        Me.btnAdd.Location = New System.Drawing.Point(105, 12)
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(263, 14)
         Me.btnAdd.Name = "btnAdd"
-        Me.btnAdd.Size = New System.Drawing.Size(48, 20)
+        Me.btnAdd.Size = New System.Drawing.Size(56, 20)
         Me.btnAdd.TabIndex = 4
         Me.btnAdd.Text = "+"
         Me.btnAdd.UseVisualStyleBackColor = True
         '
         'btnSub
         '
-        Me.btnSub.Location = New System.Drawing.Point(105, 38)
+        Me.btnSub.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSub.Location = New System.Drawing.Point(263, 40)
         Me.btnSub.Name = "btnSub"
-        Me.btnSub.Size = New System.Drawing.Size(48, 23)
+        Me.btnSub.Size = New System.Drawing.Size(56, 23)
         Me.btnSub.TabIndex = 5
         Me.btnSub.Text = "-"
         Me.btnSub.UseVisualStyleBackColor = True
         '
         'btnMul
         '
-        Me.btnMul.Location = New System.Drawing.Point(105, 67)
+        Me.btnMul.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMul.Location = New System.Drawing.Point(263, 69)
         Me.btnMul.Name = "btnMul"
-        Me.btnMul.Size = New System.Drawing.Size(48, 23)
+        Me.btnMul.Size = New System.Drawing.Size(56, 23)
         Me.btnMul.TabIndex = 6
         Me.btnMul.Text = "x"
         Me.btnMul.UseVisualStyleBackColor = True
@@ -96,17 +105,18 @@ Partial Class Form1
         'lblOutput
         '
         Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblOutput.Location = New System.Drawing.Point(32, 98)
+        Me.lblOutput.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput.Location = New System.Drawing.Point(12, 14)
         Me.lblOutput.Name = "lblOutput"
-        Me.lblOutput.Size = New System.Drawing.Size(210, 23)
+        Me.lblOutput.Size = New System.Drawing.Size(245, 23)
         Me.lblOutput.TabIndex = 7
         Me.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(274, 135)
+        Me.ClientSize = New System.Drawing.Size(332, 104)
         Me.Controls.Add(Me.lblOutput)
         Me.Controls.Add(Me.btnMul)
         Me.Controls.Add(Me.btnSub)
@@ -115,6 +125,7 @@ Partial Class Form1
         Me.Controls.Add(Me.txtFirstNumber)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.Name = "Form1"
---
+++ Homework/Assignment2/Calculator/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1 ' *** Victor Velasco ***
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click, MyBase.Load
+        ' Addition
+        Dim firstNum, secondNum, result As Double
+
+        If txtFirstNumber.Text = String.Empty Or txtSecondNumber.Text = "" Then ' Empty?
+            MessageBox.Show("Please enter a number in both fields and try again.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error) ' Error!
+        Else
+            If Double.TryParse(txtFirstNumber.Text, firstNum) And Double.TryParse(txtSecondNumber.Text, secondNum) Then
+                result = firstNum + secondNum
+                lblOutput.Text = String.Format("{0} + {1} = {2}", firstNum, secondNum, result)
+            Else
+                MessageBox.Show("Please check the entries and try again.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation) ' Can't parse
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' MessageBox.Show("Enter two numbers to begin.", "Welcome!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub txtFirstNumber_TextChanged(sender As Object, e As EventArgs) Handles txtFirstNumber.TextChanged, txtSecondNumber.TextChanged
+        lblOutput.Text = "" ' Clear results when the inputs change
+    End Sub
+
+    Private Sub btnSub_Click(sender As Object, e As EventArgs) Handles btnSub.Click
+        ' Subtraction
+        Dim firstNum, secondNum, result As Double
+
+        If txtFirstNumber.Text = String.Empty Or txtSecondNumber.Text = "" Then ' Empty?
+            MessageBox.Show("Please enter a number in both fields and try again.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error) ' Error!
+        Else
+            If Double.TryParse(txtFirstNumber.Text, firstNum) And Double.TryParse(txtSecondNumber.Text, secondNum) Then
+                result = firstNum - secondNum
+                lblOutput.Text = String.Format("{0} - {1} = {2}", firstNum, secondNum, result)
+            Else
+                MessageBox.Show("Please check the entries and try again.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation) ' Can't parse
+            End If
+        End If
+    End Sub
+
+    Private Sub btnMul_Click(sender As Object, e As EventArgs) Handles btnMul.Click
+        ' Multiplication
+        Dim firstNum, secondNum, result As Double
+
+        If txtFirstNumber.Text = String.Empty Or txtSecondNumber.Text = "" Then ' Empty?
+            MessageBox.Show("Please enter a number in both fields and try again.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error) ' Error!
+        Else
+            If Double.TryParse(txtFirstNumber.Text, firstNum) And Double.TryParse(txtSecondNumber.Text, secondNum) Then
+                result = firstNum * secondNum
+                lblOutput.Text = String.Format("{0} x {1} = {2}", firstNum, secondNum, result)
+            Else
+                MessageBox.Show("Please check the entries and try again.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation) ' Can't parse
+            End If
+        End If
+    End Sub
+End Class
