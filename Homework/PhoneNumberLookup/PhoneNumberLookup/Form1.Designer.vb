451b41d693da22080b3e7d3d40911174b6fe3e5c
Victor Velasco <victor.velasco@student.swic.edu>
2019-04-04 18:53:27 -0500

    Apr 4 Update

---
+++ Homework/PhoneNumberLookup/PhoneNumberLookup/Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.grpAdd = New System.Windows.Forms.GroupBox()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.lblPhone = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.grpSearch = New System.Windows.Forms.GroupBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.chkCase = New System.Windows.Forms.CheckBox()
+        Me.lblItems = New System.Windows.Forms.Label()
+        Me.grpAdd.SuspendLayout()
+        Me.grpSearch.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(158, 93)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(57, 30)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(127, 152)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(100, 48)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSearch.Location = New System.Drawing.Point(18, 152)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(100, 48)
+        Me.btnSearch.TabIndex = 2
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'grpAdd
+        '
+        Me.grpAdd.Controls.Add(Me.lblItems)
+        Me.grpAdd.Controls.Add(Me.txtPhone)
+        Me.grpAdd.Controls.Add(Me.lblPhone)
+        Me.grpAdd.Controls.Add(Me.txtName)
+        Me.grpAdd.Controls.Add(Me.lblName)
+        Me.grpAdd.Controls.Add(Me.btnAdd)
+        Me.grpAdd.Location = New System.Drawing.Point(12, 12)
+        Me.grpAdd.Name = "grpAdd"
+        Me.grpAdd.Size = New System.Drawing.Size(226, 134)
+        Me.grpAdd.TabIndex = 0
+        Me.grpAdd.TabStop = False
+        Me.grpAdd.Text = "Add Name"
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(65, 65)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(150, 20)
+        Me.txtPhone.TabIndex = 1
+        '
+        'lblPhone
+        '
+        Me.lblPhone.AutoSize = True
+        Me.lblPhone.Location = New System.Drawing.Point(20, 67)
+        Me.lblPhone.Name = "lblPhone"
+        Me.lblPhone.Size = New System.Drawing.Size(41, 13)
+        Me.lblPhone.TabIndex = 5
+        Me.lblPhone.Text = "Phone:"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(65, 29)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(150, 20)
+        Me.txtName.TabIndex = 0
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(20, 32)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(38, 13)
+        Me.lblName.TabIndex = 4
+        Me.lblName.Text = "Name:"
+        '
+        'grpSearch
+        '
+        Me.grpSearch.Controls.Add(Me.chkCase)
+        Me.grpSearch.Controls.Add(Me.lstOutput)
+        Me.grpSearch.Controls.Add(Me.txtSearch)
+        Me.grpSearch.Location = New System.Drawing.Point(244, 12)
+        Me.grpSearch.Name = "grpSearch"
+        Me.grpSearch.Size = New System.Drawing.Size(316, 247)
+        Me.grpSearch.TabIndex = 1
+        Me.grpSearch.TabStop = False
+        Me.grpSearch.Text = "Search Names"
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.DarkRed
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.Color.White
+        Me.btnClear.Location = New System.Drawing.Point(18, 206)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(209, 42)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear EVERYTHING"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'lstOutput
+        '
+        Me.lstOutput.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.ItemHeight = 14
+        Me.lstOutput.Location = New System.Drawing.Point(10, 60)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(293, 172)
+        Me.lstOutput.TabIndex = 1
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(10, 29)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(147, 20)
+        Me.txtSearch.TabIndex = 0
+        '
+        'chkCase
+        '
+        Me.chkCase.AutoSize = True
+        Me.chkCase.Location = New System.Drawing.Point(163, 31)
+        Me.chkCase.Name = "chkCase"
+        Me.chkCase.Size = New System.Drawing.Size(147, 17)
+        Me.chkCase.TabIndex = 1
+        Me.chkCase.Text = "Case-Sensitive Searching"
+        Me.chkCase.UseVisualStyleBackColor = True
+        '
+        'lblItems
+        '
+        Me.lblItems.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblItems.Location = New System.Drawing.Point(6, 93)
+        Me.lblItems.Name = "lblItems"
+        Me.lblItems.Size = New System.Drawing.Size(146, 30)
+        Me.lblItems.TabIndex = 5
+        Me.lblItems.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnAdd
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(576, 270)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.grpSearch)
+        Me.Controls.Add(Me.grpAdd)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.btnExit)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Phonebook"
+        Me.grpAdd.ResumeLayout(False)
+        Me.grpAdd.PerformLayout()
+        Me.grpSearch.ResumeLayout(False)
+        Me.grpSearch.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents grpAdd As GroupBox
+    Friend WithEvents txtPhone As TextBox
+    Friend WithEvents lblPhone As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents lblName As Label
+    Friend WithEvents grpSearch As GroupBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lstOutput As ListBox
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents chkCase As CheckBox
+    Friend WithEvents lblItems As Label
+End Class
---
+++ Homework/PhoneNumberLookup/PhoneNumberLookup/Form1.vb
@@ -0,0 +1,117 @@
+﻿Option Strict On ' *** Victor Velasco ***
+
+Public Class Form1
+    Dim names(-1), phones(-1) As String ' Array that will hold info
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If txtName.Text = "" Or txtPhone.Text = "" Then
+            NotifyUser("Please enter a name and phone number.", "err")
+        Else
+            ' Adds one to both arrays
+            ReDim Preserve names(names.Length)
+            ReDim Preserve phones(phones.Length)
+
+            ' Add entry to array
+            names(names.Length - 1) = txtName.Text
+            phones(phones.Length - 1) = txtPhone.Text
+
+            ' Clear the TextBoxes and return focus
+            txtName.Clear()
+            txtPhone.Clear()
+            txtName.Focus()
+            PhoneBookEntries() ' NotifyUser("Added successfully!", "info")
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close() ' Closes form
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Dim userSearch As String
+        Dim flag As Boolean = False
+
+        ' Get user input
+        userSearch = InputBox("Enter a partial or full name to search for.")
+
+        ' Loop through array
+        For i = 0 To names.Length - 1
+            If names(i).ToUpper().Contains(userSearch.ToUpper()) Then ' Does it match?
+                NotifyUser(String.Format("{0} {1}", names(i), phones(i)), "info") ' MessageBox to user
+                flag = True
+                Exit For ' Only need the first occurance
+            End If
+        Next
+
+        If flag = False Then NotifyUser("Name not found.", "info") ' Not found
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        Dim msgReturn As Integer ' Holds the return value of the MessageBox
+
+        ' Confirm with the user
+        msgReturn = MessageBox.Show("Are you sure you want to erase EVERYTHING?" & vbCrLf & vbCrLf & "This action cannot be undone.", "WARNING!", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If msgReturn = DialogResult.Yes Then ' Delete entries?
+            ' Clears the arrays
+            ReDim names(-1)
+            ReDim phones(-1)
+
+            ' Clear out the rest of the form too
+            txtName.Clear()
+            txtPhone.Clear()
+            txtSearch.Clear()
+            lstOutput.Items.Clear()
+            chkCase.Checked = False
+            PhoneBookEntries()
+
+            ' Notify user that it was done
+            NotifyUser("Everything has been erased.", "info")
+        Else
+            ' The user did not want to clear 
+            NotifyUser("Nothing was erased.", "info")
+            PhoneBookEntries()
+        End If
+    End Sub
+
+    Private Sub txtSearch_TextChanged(sender As Object, e As EventArgs) Handles txtSearch.TextChanged
+        If txtSearch.Text = String.Empty Then ' Empty field?
+            lstOutput.Items.Clear() ' Search TextBox is empty
+        Else
+            lstOutput.Items.Clear() ' We need to clear the previous results
+
+            If chkCase.Checked Then ' Case-Sensitive
+                ' Loop through array
+                For i = 0 To names.Length - 1
+                    If names(i).Contains(txtSearch.Text) Then ' Does it match?
+                        lstOutput.Items.Add(String.Format("{0} {1}", names(i).PadRight(20), phones(i))) ' Add to ListBox
+                    End If
+                Next
+            Else ' Case-Insensitive
+                ' Loop through array
+                For i = 0 To names.Length - 1
+                    If names(i).ToUpper().Contains(txtSearch.Text.ToUpper()) Then ' Does it match?
+                        lstOutput.Items.Add(String.Format("{0} {1}", names(i).PadRight(20), phones(i))) ' Add to ListBox
+                    End If
+                Next
+            End If
+        End If
+    End Sub
+
+    Sub NotifyUser(msg As String, icon As String) 'Handles MessageBox
+        Select Case icon
+            Case "err"
+                MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Case "info"
+                MessageBox.Show(msg, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Select
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PhoneBookEntries()
+    End Sub
+
+    Sub PhoneBookEntries()
+        lblItems.Text = "# of Entries: " & names.Count()
+    End Sub
+End Class
